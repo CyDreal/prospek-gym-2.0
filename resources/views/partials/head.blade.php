3aee038b53b99dab58a0da8ac39c83e65dfd6eab
CyDreal <ditodiaz25@gmail.com>
2025-06-07 00:06:12 +0700

    fix: bug mobile button didn't appear on /about

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <title>@yield('title', 'Prospek Gym')</title>
@@ -10,4 +10,3 @@
 
 <!-- Scripts -->
 @vite(['resources/css/app.css', 'resources/js/app.js'])
-<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
